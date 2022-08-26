c265e312eddd9bec31c2ba8be261138eb9e6d3ac
Filipe Vidal <vidaltecst@gmail.com>
2022-08-25 21:25:19 -0300

    login

---
+++ web_agendaqui/src/Pages/Adm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Adm() {
+    return (
+        <div>Página do administrador</div>
+    )
+}
--- web_agendaqui/src/Pages/Login.js
+++ web_agendaqui/src/Pages/Login.js
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import api from "../service/api";
 
 export default function Login() {
+    const [erro, setErro] = useState('')
+    const [dadosLogin, setDadosLogin] = useState({
+        email: '',
+        senha: ''
+    })
+
+    const pegarDadosLogin = (e) => {
+        setErro('')
+        setDadosLogin({...dadosLogin, [e.target.name]: e.target.value})
+    }
+
+    const logar = () => {
+        const body = {
+            email: dadosLogin.email,
+            password: dadosLogin.senha
+        }
+        api.post('/api/login', body, {
+            headers: {
+                Accept: 'application/json'
+            }
+        }).then((res) => {
+            console.log(res.data.token)
+            localStorage.setItem('token', res.data.token)
+        }).catch((err) => {
+            console.log(err.response.data.erro)
+            setErro(err.response.data.erro)
+        })
+    }
+
     return (
         <div className="login">
             <h1>Entrar</h1>
             <div className="form">
-                <input placeholder="telefone"/>
-                <input placeholder="senha"/>
-                <button>Entrar</button>
+                <input name="email" value={dadosLogin.email} placeholder="email" onChange={pegarDadosLogin}/>
+                <input type="password" name="senha" value={dadosLogin.senha} placeholder="senha" onChange={pegarDadosLogin}/>
+                <p>{erro}</p>
+                <button onClick={logar}>Entrar</button>
             </div>
         </div>
     )
